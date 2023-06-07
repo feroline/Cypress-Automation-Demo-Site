7a29cf6fe0c27f136a4e611b5cabb57afae91df0
Ana Carolina R Rocha <anacarolina.rrocha@gmail.com>
2023-06-07 16:38:01 -0300

    alterando o site de automação

---
+++ cypress/e2e/Login.cy.js
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker/locale/pt_BR';
+import {registerLocator} from '../support/locators/registerLocator';
+
+
+describe('Login', () => {
+  it('Valid Data', () => {
+    cy.visit('/');
+  });
+
+});
