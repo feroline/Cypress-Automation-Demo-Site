b0f30ae72c8bf1d3b5f8aa154972e62e5023a4b4
Ana Carolina R Rocha <anacarolina.rrocha@gmail.com>
2023-06-19 15:54:30 -0300

    adicionando verificação para remover

--- cypress/e2e/buying.cy.js
+++ cypress/e2e/buying.cy.js
@@ -13,7 +13,7 @@ describe('Buying itens', () => {
     
     describe('Add to cart', () => {
 
-        it('Add to cart from the home page', () => {
+        it('Verifying the change of the button', () => {
 
             cy.get('@products').each(product => {
                 cy.get('.inventory_item').each($el => {
@@ -29,23 +29,51 @@ describe('Buying itens', () => {
                             .find('button')
                             .should('have.text','Add to cart')
                             .click();
+                   
+                        
+                        cy.wrap($el)
+                            .find('.pricebar')
+                            .first()
+                            .find('button')
+                            .should('have.text','Remove')
+   
                     }
                 });
             });
+        });
 
-            // cy.get('.shopping_cart_link').click();
-            // cy.url().should('include', '/cart.html');
+        it('Valid if the products go to cart', () => {
 
-            // cy.get('.inventory_item_name').should('have.text','Sauce Labs Backpack');
+            //add to cart
+            cy.get('@products').each(product => {
+                cy.get('.inventory_item').each($el => {
+                    
+                    let name = $el
+                        .find('.inventory_item_description')
+                        .find('.inventory_item_name');
+                
+                        if(name.text() == product){
+                            cy.wrap($el)
+                                .find('.pricebar')
+                                .first()
+                                .find('button')
+                                .click();
+                        }
+                    });
+                });
 
-            // cy.get('.pricebar')
-            //     .first()
-            //     .find('button')
-            //     .as('removeButton')
-            //     .should('have.text','Remove')
-            //     .should('have.attr','data-test','remove-sauce-labs-backpack')
+                cy.get('.shopping_cart_link').click();
+                cy.url().should('include', '/cart.html');
+            
+                cy.get('.cart_item_label').each($el => {
 
+                    let name = $el.find('a').text();
+                    cy.get('@products').should('to.include', name);
+                    
+                });
 
         });
+
+       
     });
 });
